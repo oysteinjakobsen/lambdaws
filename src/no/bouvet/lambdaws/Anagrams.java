2489c3bb260ce13a771e868874103f6b1127c0fc
oysteinjakobsen <ojapublic@manglerud.net>
2013-11-22 16:25:14 +0100

    Forbedret anagram-eksemplet

--- src/no/bouvet/lambdaws/Anagrams.java
+++ src/no/bouvet/lambdaws/Anagrams.java
@@ -3,6 +3,7 @@ package no.bouvet.lambdaws;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -29,22 +30,26 @@ public class Anagrams {
         }
     }
 
-    private static final String[] WORDS = new String[] {"Sol", "Rørt", "Tørr", "Los", "Gul", "Trør"};
+    private static final String[] WORDS = new String[]{"Sol", "Rørt", "Tørr", "Los", "Gul", "Trør"};
 
     public static void main(String... args) {
         Map<String, List<Word>> wordsGroupedByKey = Stream.of(WORDS)
                 .map(Word::new)
                 .collect(groupingBy(Word::getKey));
 
-        Predicate<List<Word>> hasMoreThanOneWord = wordList -> wordList.size() > 1;
+        Predicate<List<Word>> listHasMoreThanOneWord = wordList -> wordList.size() > 1;
 
         List<String> anagrams = wordsGroupedByKey.entrySet().stream()
-                .map(entry -> entry.getValue())
-                .filter(hasMoreThanOneWord)
+                .map(entryToListOfWords())
+                .filter(listHasMoreThanOneWord)
                 .map(Anagrams::wordListToCommaSeparatedString)
                 .collect(toList());
 
         anagrams.forEach(System.out::println);
+    }
+
+    private static Function<Map.Entry<String, List<Word>>, List<Word>> entryToListOfWords() {
+        return entry -> entry.getValue();
     }
 
     private static String wordListToCommaSeparatedString(List<Word> wordList) {
